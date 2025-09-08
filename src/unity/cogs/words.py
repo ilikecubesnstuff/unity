1ddd628e9e6549eac4b7b4667734fb8d613ed9ff
ilikecubesnstuff <25328250+ilikecubesnstuff@users.noreply.github.com>
2025-09-08 03:24:48 +0200

    Improve dictionary API commands and fix related bugs.

--- src/unity/cogs/words.py
+++ src/unity/cogs/words.py
@@ -5,95 +5,102 @@ import discord
 from discord.ext import commands, tasks
 from discord.ext.pages import Page, Paginator
 
-from ..util.merweb import MerWebWrapper, mw_dict_link, processed
+from ..util.merweb import ICON, MerWebWrapper, mw_dict_link, processed
 
 logger = logging.getLogger(__name__)
 
 
-def definition_line(sn: str, dt: str) -> str:
+# definition line
+def def_ln(sn: str, dt: str) -> str:
     if not dt:
         return ""
     indent = ""
     if not sn:
-        return f"{dt}\n"
+        return f"{dt}"
     elif sn.split()[0] in "abcdefghijklmnopqrstuvwxyz":
         indent = "  "
     elif sn.split()[0].startswith("("):
         indent = "    "
-    return f"**`{indent}{sn}`**{dt}\n"
+    return f"**`{indent}{sn}`**{dt}"
 
 
-def extract_pages_from_response(query, response):
+def extract_page_from_entry(entry):
+    # basic info
+    word = entry["hwi"]["hw"].replace("*", "")
+    fl = f" ({entry['fl']})" if "fl" in entry else ""
+
+    # set up embed description with pronunciation if available
+    description = f"-# {word}"
+    if "prs" in entry["hwi"]:
+        prs = entry["hwi"]["prs"]
+        pr_strs = [pr["mw"] for pr in prs if "mw" in pr]
+        if pr_strs:
+            description = f"-# {word} /{', '.join(pr_strs)}/"
+        else:
+            description = f"-# {word}"
+
+    # construct main text with definitions
+    lines = [f"## {mw_dict_link(word)}{fl}"]
+    for sseq in entry["def"]:
+        if "vd" in sseq:
+            lines.append(f"-# {sseq['vd']}")
+        for sense in sseq["sseq"]:
+            for subsense in sense:
+                if subsense[0] == "sense" or subsense[0] == "sen":
+                    info = subsense[1]
+                    data = info.get("dt", []) or info.get("et", [])
+                    for el in data:
+                        if el[0] == "text":
+                            lines.append(def_ln(info.get("sn", ""), el[1]))
+                            break
+                elif subsense[0] == "pseq":
+                    ps_info = subsense[1]
+                    for name, part in ps_info:
+                        if name == "bs":
+                            info = part["sense"]
+                            data = info.get("dt", []) or info.get("et", [])
+                            for el in data:
+                                if el[0] == "text":
+                                    lines.append(def_ln(info.get("sn", ""), el[1]))
+                                    break
+                        elif name == "sense" or name == "sen":
+                            info = part
+                            data = info.get("dt", []) or info.get("et", [])
+                            for el in data:
+                                if el[0] == "text":
+                                    lines.append(def_ln(info.get("sn", ""), el[1]))
+                                    break
+        lines.append("")
+
+    # construct embed
     embed = discord.Embed(
+        color=discord.Color.blue(),
+        description=description,
+        timestamp=discord.utils.utcnow(),
         footer=discord.EmbedFooter(
             text="Data from Merriam-Webster",
-            icon_url="https://dictionaryapi.com/images/info/branding-guidelines/MWLogo_DarkBG_120x120_2x.png",
+            icon_url=ICON,
         ),
-        color=discord.Color.blue(),
-        timestamp=discord.utils.utcnow(),
     )
 
-    if not response:
-        embed.description = f"No definitions found for **{query}**."
-        embed.color = discord.Color.red()
-        return [embed]
-
-    if isinstance(response[0], str):
-        suggestions = ", ".join(response)
-        embed.description = (
-            f"No exact match found for **{query}**. *Did you mean: {suggestions}?*"
+    # add etymology field
+    if "et" in entry:
+        et_text = "\n".join(item[1] for item in entry["et"] if item[0] == "text")
+        embed.add_field(
+            name="Etymology",
+            value=processed(et_text),
+            inline=False,
         )
-        embed.color = discord.Color.red()
-        return [embed]
 
-    pages = []
-    for data in response:
-        page_embed = embed.copy()
-        word = data["hwi"]["hw"].replace("*", "")
-        text = f"## [{word}]({mw_dict_link(word)}) ({data['fl']})\n"
+    # add first known use field
+    if "date" in entry:
+        embed.add_field(
+            name="First Known Use",
+            value=processed(entry["date"]),
+            inline=False,
+        )
 
-        for sseq in data["def"]:
-            for sense in sseq["sseq"]:
-                for subsense in sense:
-                    dt = ""
-                    sn = ""
-                    if subsense[0] == "sense":
-                        sense_data = subsense[1]
-                        sn = sense_data.get("sn", sn)
-                        for el in sense_data["dt"]:
-                            if el[0] == "text":
-                                dt = el[1]
-                                break
-                        text += definition_line(sn, dt)
-                    elif subsense[0] == "pseq":
-                        sense_data = subsense[1]
-                        for name, part in sense_data:
-                            if name == "bs":
-                                sn = part["sense"].get("sn", sn)
-                                for el in part["sense"]["dt"]:
-                                    if el[0] == "text":
-                                        dt = el[1]
-                                        break
-                            elif name == "sense":
-                                sn = part.get("sn", sn)
-                                for el in part["dt"]:
-                                    if el[0] == "text":
-                                        dt = el[1]
-                                        break
-                            text += definition_line(sn, dt)
-        if "pr" in data["hwi"]:
-            page_embed.description = f"-# {data['hwi']['hw']} /{data['hwi']['pr']}/"
-        if "et" in data:
-            body = [item[1] for item in data["et"] if item[0] == "text"][
-                0
-            ]  # usually only one
-            page_embed.add_field(name="Etymology", value=processed(body), inline=False)
-        if "date" in data:
-            page_embed.add_field(
-                name="First Known Use", value=processed(data["date"]), inline=False
-            )
-        pages.append(Page(content=processed(text), embeds=[page_embed]))
-    return pages
+    return Page(content=processed("\n".join(lines)), embeds=[embed])
 
 
 class Words(commands.Cog):
@@ -116,13 +123,14 @@ class Words(commands.Cog):
     async def define_word(self, ctx, query: str):
         """Fetch and display the definition of a word from Merriam-Webster."""
         if self.mw_collegiate_counter >= 500:
+            logger.warning(
+                f"Daily request limit reached for collegiate dictionary. {self.mw_collegiate_counter = }"
+            )
             await ctx.respond("Daily request limit reached for collegiate dictionary.")
             return
         self.mw_collegiate_counter += 1
         response = await self.mw_collegiate.fetch(query)
-        pages = extract_pages_from_response(query, response)
-        paginator = Paginator(pages)
-        await paginator.respond(ctx.interaction)
+        await self.handle_response(ctx, query, response)
 
     @discord.slash_command(
         name="meddefine", description="Get the medical definition of a word"
@@ -134,7 +142,29 @@ class Words(commands.Cog):
             return
         self.mw_medical_counter += 1
         response = await self.mw_medical.fetch(query)
-        pages = extract_pages_from_response(query, response)
+        await self.handle_response(ctx, query, response)
+
+    async def handle_response(self, ctx, query, response):
+        if not response:
+            await ctx.respond(
+                embed=discord.Embed(
+                    description=f"No definitions found for {query}.",
+                    color=discord.Color.red(),
+                )
+            )
+            return
+
+        if isinstance(response[0], str):
+            suggestions = ", ".join(response)
+            await ctx.respond(
+                embed=discord.Embed(
+                    description=f"No exact match found for {query}. *Did you mean: {suggestions}?*",
+                    color=discord.Color.red(),
+                )
+            )
+            return
+
+        pages = [extract_page_from_entry(entry) for entry in response]
         paginator = Paginator(pages)
         await paginator.respond(ctx.interaction)
 
--- src/unity/util/merweb.py
+++ src/unity/util/merweb.py
@@ -2,6 +2,8 @@ import re
 
 import aiohttp
 
+ICON = "https://dictionaryapi.com/images/info/branding-guidelines/MWLogo_DarkBG_120x120_2x.png"
+
 
 class MerWebWrapper:
     def __init__(self, base_url: str, api_key: str = None):
@@ -9,7 +11,7 @@ class MerWebWrapper:
         self.api_key = api_key
 
     async def fetch(self, word: str):
-        url = f"{self.base_url}/{word}"
+        url = f"{self.base_url}/{word.replace(' ', '%20')}"
         if self.api_key:
             url += f"?key={self.api_key}"
 
@@ -21,72 +23,52 @@ class MerWebWrapper:
 
 
 def mw_dict_link(hw: str) -> str:
-    return f"https://www.merriam-webster.com/dictionary/{hw.replace(' ', '%20')}"
+    return (
+        f"[{hw}](https://www.merriam-webster.com/dictionary/{hw.replace(' ', '%20')})"
+    )
+
+
+# following https://dictionaryapi.com/products/json
+processing_map = {
+    # formatting and punctuation
+    r"\{b\}(.*?)\{/b\}": lambda m: "**" + m.group(1) + "**",  # bold
+    r"\{bc\}": lambda m: ": ",  # bold colon
+    r"\{inf\}(.*?)\{/inf\}": lambda m: m.group(1),  # inflection
+    r"\{it\}(.*?)\{/it\}": lambda m: "*" + m.group(1) + "*",  # italics
+    r"\{ldquo\}": lambda m: "“",  # left double quote
+    r"\{rdquo\}": lambda m: "”",  # right double quote
+    r"\{sc\}(.*?)\{/sc\}": lambda m: m.group(1).upper(),  # small caps
+    r"\{sup\}(.*?)\{/sup\}": lambda m: m.group(1),  # superscript
+    # word-marking and gloss
+    r"\{gloss\}(.*?)\{/gloss\}": lambda m: "[" + m.group(1) + "]",  # gloss
+    r"\{parahw\}(.*?)\{/parahw\}": lambda m: "**" + m.group(1).upper() + "**",  # headword instance in paragraph
+    r"\{phrase\}(.*?)\{/phrase\}": lambda m: "***" + m.group(1) + "***",  # phrase
+    r"\{qword\}(.*?)\{/qword\}": lambda m: "*" + m.group(1) + "*",  # headword instance in quote
+    r"\{wi\}(.*?)\{/wi\}": lambda m: "*" + m.group(1) + "*",  # headword instance in running text
+    # cross-reference grouping
+    r"\{dx\}(.*?)\{/dx\}": lambda m: "—" + m.group(1),  # introductory text
+    r"\{dx_def\}(.*?)\{/dx_def\}": lambda m: "—" + m.group(1),  # parenthetical xref with introductory text
+    r"\{dx_ety\}(.*?)\{/dx_ety\}": lambda m: "—" + m.group(1),  # directional xref within etymology
+    r"\{ma\}(.*?)\{/ma\}": lambda m: "—more at " + m.group(1),  # more at
+    # cross-reference links
+    r"\{a_link\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # auto link
+    r"\{d_link\|(.*?)\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # direct link
+    r"\{i_link\|(.*?)\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # italicized link
+    r"\{et_link\|(.*?)\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # etymology link
+    r"\{mat\|(.*?)\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # more at target
+    r"\{sx\|(.*?)\|(.*?)\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # synonymous xref
+    r"\{dxt\|(.*?):(.*?)\|(.*?)\|(.*?)\}": lambda m: mw_dict_link(m.group(1)),  # directional xref target
+    # date sense
+    r"\{ds\|(.*?)\|(.*?)\|(.*?)\}": lambda m: "",  # date sense
+}
 
 
 def processed(text: str) -> str:
-    # following https://dictionaryapi.com/products/json
-    text = re.sub(r"\{b\}(.*?)\{/b\}", lambda m: "**" + m.group(1) + "**", text)
-    text = re.sub(r"\{bc\}", ": ", text)
-    text = re.sub(r"\{inf\}(.*?)\{/inf\}", lambda m: m.group(1), text)
-    text = re.sub(r"\{it\}(.*?)\{/it\}", lambda m: "*" + m.group(1) + "*", text)
-    text = re.sub(r"\{ldquo\}", "“", text)
-    text = re.sub(r"\{rdquo\}", "”", text)
-    text = re.sub(r"\{sc\}(.*?)\{/sc\}", lambda m: m.group(1).upper(), text)
-    text = re.sub(r"\{sup\}(.*?)\{/sup\}", lambda m: m.group(1), text)
+    if not text:
+        return text
 
-    text = re.sub(r"\{gloss\}(.*?)\{/gloss\}", lambda m: "[" + m.group(1) + "]", text)
-    text = re.sub(
-        r"\{parahw\}(.*?)\{/parahw\}",
-        lambda m: "\n**" + m.group(1).upper() + "**",
-        text,
-    )
-    text = re.sub(
-        r"\{phrase\}(.*?)\{/phrase\}", lambda m: "***" + m.group(1) + "***", text
-    )
-    text = re.sub(r"\{qword\}(.*?)\{/qword\}", lambda m: "*" + m.group(1) + "*", text)
-    text = re.sub(r"\{wi\}(.*?)\{/wi\}", lambda m: "*" + m.group(1) + "*", text)
-
-    text = re.sub(r"\{dx\}(.*?)\{/dx\}", lambda m: "—" + m.group(1), text)
-    text = re.sub(r"\{dx_def\}(.*?)\{/dx_def\}", lambda m: "—" + m.group(1), text)
-    text = re.sub(r"\{dx_ety\}(.*?)\{/dx_ety\}", lambda m: "—" + m.group(1), text)
-    text = re.sub(r"\{ma\}(.*?)\{/ma\}", lambda m: "—more at " + m.group(1), text)
-
-    text = re.sub(
-        r"\{a_link\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(
-        r"\{d_link\|(.*?)\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(
-        r"\{i_link\|(.*?)\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(
-        r"\{d_link\|(.*?)\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(
-        r"\{mat\|(.*?)\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(
-        r"\{sx\|(.*?)\|(.*?)\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(
-        r"\{dxt\|(.*?):(.*?)\|(.*?)\|(.*?)\}",
-        lambda m: f"[{m.group(1)}]({mw_dict_link(m.group(1))})",
-        text,
-    )
-    text = re.sub(r"\{ds\|(.*?):(.*?)\|(.*?)\|(.*?)\}", "", text)
+    # Apply all regex substitutions in the processing_map
+    for pattern, repl in processing_map.items():
+        text = re.sub(pattern, repl, text)
 
     return text
